a4d5686b9487d1485ae400cb376cc004738271a6
parkj <parkj@172.17.182.102>
2021-09-23 17:52:10 +0900

    Modified menu

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -5,14 +5,15 @@ public class Menu {
     {
         System.out.println();
         System.out.println("================ To-do List ================");
-        System.out.println("1. Add a new item ( add )");
-        System.out.println("2. Delete an existing item ( del )");
-        System.out.println("3. Update an item  ( edit )");
-        System.out.println("4. List all items ( ls )");
-        System.out.println("5. sort the list by name ( ls_name_asc )");
-        System.out.println("6. sort the list by name ( ls_name_desc )");
-        System.out.println("7. sort the list by date ( ls_date )");
-        System.out.println("8. exit (Or press escape key to exit)");
+        System.out.println("add -  Add a new item");
+        System.out.println("del -  Delete an existing item");
+        System.out.println("edit - Update an item");
+        System.out.println("ls - List all items");
+        System.out.println("ls_name_asc - Sort the list by name");
+        System.out.println("ls_name_desc - Sort the list by name");
+        System.out.println("ls_date - Sort the list by date");
+        System.out.println("help - Shows the menu");
+        System.out.println("exit - Exit (Or press escape key to exit)");
     }
     
     public static void prompt() {
